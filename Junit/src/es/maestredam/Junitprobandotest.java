c970efacb80e69c0b19c7a79570b5fa28c12ad98
Esteban <Esteban@DESKTOP-NSF0L7A.home>
2021-05-21 13:16:59 +0200

    proyecto para hacer pruebas unitarias

---
+++ Junit/src/es/maestredam/Junitprobandotest.java
@@ -0,0 +1,14 @@
+package es.maestredam;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class Junitprobandotest {
+
+	@Test
+	public void test() {
+		fail("Not yet implemented");
+	}
+
+}
