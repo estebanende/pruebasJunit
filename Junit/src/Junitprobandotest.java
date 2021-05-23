2949d6e0a39616f29d592a99b6ef437b6913006b
Esteban <Esteban@DESKTOP-NSF0L7A.home>
2021-05-23 10:42:49 +0200

    hemos hecho todas las pruebas

--- Junit/src/Junitprobandotest.java
+++ Junit/src/Junitprobandotest.java
@@ -9,19 +9,64 @@ public class Junitprobandotest {
 
 	public static void mai(String[] args) {
 		
-		JUnitCore.runClasses(VehiculosApp.class);
+		JUnitCore.runClasses(Junitprobandotest.class);
 		
 		
 		
 	}
-	@Test
+	/*@Test
 	public void test() {
 		Vehiculo pr1= new Vehiculo("0423","seat", "sara");
 		
 		Assert.assertEquals("mala " , pr1.getModelo() );
+		*/
+	//}
+	@Test
+	public void testWrite() {
+		Vehiculo v1=new Vehiculo("CFC 6666","BMW","X3");
+		EscritorObjetos eo1=new EscritorObjetos("v1.obt");
+		System.out.println("probando write() : ");
+		Assert.assertEquals(1, eo1.write(v1));
+				
+				
+	}
+	@Test
+	public void testeandoadd() {
+		Vehiculo v1=new Vehiculo("CFC 6666","BMW","X3");
+		EscritorObjetos eo1=new EscritorObjetos("v1.obt");
+		System.out.println("probando write() : ");
+		Assert.assertEquals(1, eo1.add(v1));
+	}
+	
+	@Test
+	public  void probandoArraytest()
+	{
+		
+		Vehiculo v1=new Vehiculo("CFC 6666","BMW","X3");
+		EscritorObjetos eo1=new EscritorObjetos("v1.obt");
+		System.out.println("probando write() : ");
+		Vehiculo arra []=new Vehiculo [4] ;
+		Assert.assertEquals(4,eo1.add(arra));
+	}
+	
+	@Test
+	public  void ProbandoWriteArra()
+	{
+		Vehiculo v1=new Vehiculo("CFC 6666","BMW","X3");
+		EscritorObjetos eo1=new EscritorObjetos("v1.obt");
+		System.out.println("probando write() : ");
+		Vehiculo arra []=new Vehiculo [4] ;
+		Assert.assertEquals(4,eo1.write(arra));
+	}
+	
+
+	@Test
+	public void leerobjetos()
+	{
 		
 	}
 	
+ 
 	
 	
 	
